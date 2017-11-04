bf6041e9a6d4d0139e40e71887637df290f88067
kang <later@later.com>
2017-11-04 19:09:33 +0900

    list study

---
+++ color_list.py
@@ -0,0 +1,15 @@
+colors = ['red', 'blue', 'yellow', 'black', 'green']
+
+how_many = len(colors)
+
+print(how_many)
+
+the_last = colors[-1]
+
+print(the_last)
+
+print(the_last[0].upper() + the_last[1:])
+
+the_last = the_last.capitalize()
+
+print(the_last)
